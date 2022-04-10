5dd7c263b7b6d931ddad4cf9011eb8adedc233b0
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2022-04-10 05:26:00 +0430

    first commit

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>asasas &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
