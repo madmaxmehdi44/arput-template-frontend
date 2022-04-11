8a357f806cdb7a524ecba24959d976bc985ce035
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2022-04-11 09:02:25 +0430

    key={article.id}

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -14,10 +14,10 @@ const Category = ({ category, categories }) => {
         
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div  className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
       {category.attributes.articles.data.map((article) => (
 
-            <Article article={article} />
+            <Article key={article.id} article={article} />
       
       ))}
         </div>
